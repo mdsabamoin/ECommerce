70eec9148f526e6f0bba84b6de331eeb7822350e
unknown <sabamoin419@gmail.com>
2024-11-19 22:45:36 +0530

    implemented homepage with router

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import CustomNavbar from './Components/CustomNavbar';
 import CustomItems from './Components/CustomItems';
 import {createBrowserRouter,RouterProvider} from "react-router-dom";
 import CustomAbout from './Components/CustomAbout';
+import Stored from './Components/Stored';
+import Card from 'react-bootstrap/Card';
 function App() {
      
   const AlwaysDisplay = <div><CustomNavbar/><h1  className="bg-secondary text-light glowing-text" style={{height:"130px",fontSize: '6rem'}}>The Generics</h1></div>
-
+  const LastContent=   <Card.Footer className="bg-info text-dark glowing-text" style={{height:"130px",marginBottom:"-20px",fontSize:"3rem"}}>Dummy Footer</Card.Footer>
        
    const router = createBrowserRouter([
-    {path:"/about" , element:<div>{AlwaysDisplay}<CustomAbout/></div>},
-    {path:"/",element:<div>{AlwaysDisplay}<CustomItems/></div>},
-    {path:"/store",element:<div>{AlwaysDisplay}<div>This is a store</div></div>}
+    {path:"/about" , element:<div>{AlwaysDisplay}<CustomAbout/>{LastContent}</div>},
+    {path:"/",element:<div>{AlwaysDisplay}<CustomItems/>{LastContent}</div>},
+    {path:"/store",element:<div>{AlwaysDisplay}<Stored/>{LastContent}</div>}
  ])
   
   return (<div className='App'>
---
+++ src/Components/Stored.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Button from 'react-bootstrap/Button';
+import { FaPlay } from 'react-icons/fa';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+const Stored = () => {
+    let arr = [{ date: "JUL16", place: "DETROIT,MI", activity: "DTE ENERGY MUSIC THEATRE" },
+    { date: "JUL19", place: "TORONTO,ON", activity: "DTE ENERGY MUSIC THEATRE" },
+    { date: "JUL20", place: "BRISTOW, VA", activity: "JIGGY LUBE LIVE" },
+    { date: "JUL25", place: "PHOENIX, AZ", activity: "AK-CHIN PAVILION" },
+    { date: "JUL27", place: "LAS VEGAS, NV", activity: "T-MOBILE ARENA" },
+    { date: "JUL28", place: "CONCORD, CA", activity: "CONCORD PAVILION" },
+    { date: "JUL16", place: "DETROIT,MI", activity: "DTE ENERGY MUSIC THEATRE" }]
+    return (<diV>
+        <div className="bg-secondary text-light glowing-text" style={{ height: "130px", margin: "-20px" }}>
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column" }}>
+                <Button variant="outline-info" style={{ height: "50px" }} className="text-light glowing-text">Get our Latest Album</Button>
+                <Button variant="outline-info" size="lg" style={{ marginTop: "15px" }}><FaPlay /></Button>
+            </div>
+        </div>
+        <Container>
+            <div style={{ marginTop: "3%" }}>
+                <h2>Tours</h2>
+                {arr.map((item) => {
+                    return <Container>
+                        <Row>
+                            <Col>{item.date}</Col>
+                            <Col>{item.place}</Col>
+                            <Col>{item.activity}</Col>
+                            <Col><Button variant="info">BUY TICKETS</Button></Col>
+
+                        </Row>
+                        <hr style={{ borderTop: "3px solid #333" }} />
+                    </Container>
+                })}
+            </div>
+        </Container>
+
+    </diV>
+
+    )
+}
+
+export default Stored;
