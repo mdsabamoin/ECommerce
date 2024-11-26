3ad51598958fbbfab1a8eae6a231bc6102e91a41
unknown <sabamoin419@gmail.com>
2024-11-26 23:10:32 +0530

    password change functionality added

--- src/Components/CustomProfile.jsx
+++ src/Components/CustomProfile.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { Context } from "../Store/ContextProvider";
+import axios from "axios";
 
 const CustomProfile = ()=>{
+      
+    const [newpass,setNewPass] = useState("");
+    const ctx = useContext(Context);
+
+    const ChangePasswordHandler = async (e)=>{
+        try{
+            e.preventDefault();
+            const obj = {"idToken":ctx.Token,"password":newpass,"returnSecureToken":true}
+            const response = await axios.post("https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyD7sUmCwmx9dDxJGu9oehaoEmJKUtqm9k8",obj)
+            console.log(response.data)
+            alert("password Changed")
+        }
+        catch(error){
+            alert(error.response.data.error.message)
+        }
+       
+
+    }
+
    return <div>
     <h1>Your User Profile</h1>
     <div style={{display:"flex",justifyContent:"center",alignItems:"center",height:"40vh"}}>
@@ -16,9 +37,11 @@ const CustomProfile = ()=>{
         id="inputPassword5"
         aria-describedby="passwordHelpBlock"
         placeholder="New Password"
+        onChange={e=>setNewPass(e.target.value)}
+        value={newpass}
       />
     </>
-        <Button variant="primary">Change Password</Button>
+        <Button variant="primary" onClick={ChangePasswordHandler}>Change Password</Button>
       </Card.Body>
     </Card>
     </div>
